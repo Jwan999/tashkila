b33ea31c07f306508fd117a7ce8f57b03b694d49
Jwan <jwanaalfatla1999@gmail.com>
2021-10-15 18:14:21 +0300

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css?ver=12')}}">
+    <link rel="stylesheet" href="css/app.css">
     {{--    <link rel="stylesheet" href="css/app.css">--}}
     <title>Asdikaee</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -84,6 +84,6 @@
     </div>
 </div>
 
-<script src="{{asset("js/app.js")}}"></script>
+<script src="js/app.js"></script>
 </body>
 </html>
